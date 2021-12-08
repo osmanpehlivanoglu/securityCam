e8fe57f21c1f1999cd0cd112f8eef2706105d2c3
OsmanPehlivanoglu <osman.pehlivanoglu@outlook.com.tr>
2021-12-08 22:27:57 +0300

    save video after detection

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-recording = True
+detection = False
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_AFTER_DETECTION = 5
+
 frame_size = (int(cap.get(3)), int(cap.get(4)))
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter("video.mp4", fourcc, 20, frame_size)
+
 
 while True:
     _, frame = cap.read()
@@ -19,9 +23,28 @@ while True:
     bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
 
     if len(faces) + len(bodies) > 0:
-        recording = True
+        if detection:
+            # birileri hala var ya da yeni birileri geldi durumu
+            # kamerayı kapat aç yapmayalım diye zamanlayıcıyı başlatmıyoruz
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("%d-%m-%y-%H-%M-%S")
+            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started Recording!")
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >= SECONDS_TO_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print("Stop Recording!")
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
 
-    out.write(frame)
+    if detection:
+        out.write(frame)
 
     # for (x, y, width, height) in faces:
     #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
